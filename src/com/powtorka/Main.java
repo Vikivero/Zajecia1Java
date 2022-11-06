fdb526f1d16297399a1526c29f58b25138e4955c
vikivero <vikichan.ch@gmail.com>
2022-11-06 16:52:33 +0100

    zadanie2

--- src/com/powtorka/Main.java
+++ src/com/powtorka/Main.java
@@ -14,11 +14,47 @@ public class Main {
         wilczur.spaceruj();
         jamnik.jedz();
         wilczur.jedz();
+
+        Pies burek = new Pies();
+        Pies pudel;
+        Pies husky =  new Pies("Dominik",70.0);
+        jamnik = new Pies(20.0, "bialy");
+        Pies wilk = new Pies(husky);
+
+
     }
 }
 
 class Pies {
     private String name;
+    private double energia;
+    private String umaszczenie;
+
+    public Pies() {
+        energia = 50.0;
+    }
+
+    public Pies(Pies source) {
+     this.umaszczenie = source.umaszczenie;
+     this.energia = source.energia;
+     this.name = source.name;
+    }
+
+    public Pies(double energiaPoczatkowa) {
+        this.energia = energiaPoczatkowa;
+    }
+
+    public Pies(String name, double energia) {
+        this.name = name;
+        this.energia = energia;
+    }
+
+    public Pies(double energiaPoczatkowa, String umaszczenie) {
+        this.energia = energiaPoczatkowa;
+        this.umaszczenie = umaszczenie;
+    }
+
+
 
     public String getName() {
         return name;
@@ -28,12 +64,22 @@ class Pies {
         this.name = name;
     }
 
+
     public void spaceruj() {
+        energia--;
         System.out.println(name + " teraz jest na spacerze.");
+        ileEnergii();
     }
 
     public void jedz() {
+        energia += 3;
         System.out.println(name + " teraz je.");
+        ileEnergii();
     }
+
+    public void ileEnergii() {
+        System.out.println(energia);
+    }
+
 }
 
